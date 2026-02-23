feacf29051c7f8b092e008fa69ce13b2f19da841
azalty <36737524+azalty@users.noreply.github.com>
2026-02-23 22:40:59 +0100

    fix compile-shop.bat

--- addons/sourcemod/scripting/compile-shop.bat
+++ addons/sourcemod/scripting/compile-shop.bat
@@ -1,4 +1,12 @@
 spcomp myjailshop.sp -o../plugins/myjailshop.smx 
+spcomp myjailshop_door.sp -o../plugins/myjailshop_door.smx 
+spcomp myjailshop_freeday.sp -o../plugins/myjailshop_freeday.smx 
+spcomp myjailshop_hud.sp -o../plugins/myjailshop_hud.smx 
+spcomp myjailshop_blackout.sp -o../plugins/myjailshop_blackout.smx 
+spcomp myjailshop_jetpack.sp -o../plugins/myjailshop_jetpack.smx 
+spcomp myjailshop_jihad.sp -o../plugins/myjailshop_jihad.smx 
+spcomp myjailshop_tacticalshield.sp -o../plugins/myjailshop_tacticalshield.smx 
+spcomp myjailshop_icegrenade.sp -o../plugins/myjailshop_icegrenade.smx 
+spcomp myjailshop_parachute.sp -o../plugins/myjailshop_parachute.smx 
 spcomp myjailshop-sm-store.sp -o../plugins/disabled/myjailshop-sm-store.smx 
-spcomp myjailshop-frozdark-shop.sp -o../plugins/disabled/myjailshop-frozdark-shop.smx 
 pause
